42b35194e23ee697450b5f5c6e1595f79b271b2d
ZL&EL <lzc123456ab@gmail.com>
2023-08-05 22:44:02 -0400

    4

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
     thePageNumber: number = 1;
-    thePageSize: number = 10;
+    thePageSize: number = 5;
     theTotalElements: number = 0;
   
   constructor(private productService: ProductService,
@@ -89,4 +89,11 @@ export class ProductListComponent implements OnInit {
   }}
     );
   }
+
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize; // use + to convert string to number
+    this.thePageNumber = 1;
+    this.listProducts();
+    }
 }
