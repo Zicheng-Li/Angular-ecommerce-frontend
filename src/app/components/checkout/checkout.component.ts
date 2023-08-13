7b2a68aede4b80d1e728f0fede8e962072105658
ZL&EL <lzc123456ab@gmail.com>
2023-08-12 21:16:08 -0400

    4

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -88,6 +88,11 @@ export class CheckoutComponent implements OnInit {
     console.log("the shipping address  country is " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
     console.log("the shipping address state is " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
   copyShipToBill(event: Event) {
     const target = event.target as HTMLInputElement;
     if (target.checked) {
