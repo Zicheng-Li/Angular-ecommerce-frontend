fb51db286dfe795a05b6d6d83cc78b8ec87fb852
ZL&EL <lzc123456ab@gmail.com>
2023-08-16 20:45:08 -0400

    4

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,10 +13,40 @@ export class LoginComponent implements OnInit {
 
   okatSignin: any;
   constructor(@Inject(OKTA_AUTH) private oktaAuth : OktaAuth ) {
-    this.okatSignin = new OktaSignIn();
+    this.okatSignin = new OktaSignIn({
+      logo : 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redrictUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true, // passing the info between our app and authorization server.
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
    }
 
   ngOnInit(): void {
+    this.okatSignin.remove();
+
+    this.okatSignin.renderEl({
+      el: '#okta-sign-in'},  // this name should be same as in html file
+      (response : any) => {
+        if(response.status === 'SUCCESS'){
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error : any) => {
+        throw error;
+      });
+      
   }
 
+
+
 }
+    
+  
+  
+
+
